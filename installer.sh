504f4c0202a5372e76cc824e250c46cc0a6dba10
Swiss professional sh!ta$$ <76264518+kevadesu@users.noreply.github.com>
2022-07-07 21:20:43 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # quick install file
 echo "Installing Paqué..."
 mkdir -p "$HOME/.paque"
@@ -6,9 +7,32 @@ mkdir -p "$HOME/.paque/packages"
 curl -sL "https://github.com/Lintine/xzpaque/releases/latest/download/xzpaque.tar.gz" | tar -x -z -C "$HOME/.paque"
 # add xzpaque to bashrc
 echo "Adding xzpaque to bashrc..."
-echo -e "source $HOME/.paque/main.sh\nxzpaque Pl all" >> "$HOME/.bashrc"
+echo -e "source $HOME/.paque/xzpaque/main.sh\nxzpaque Pl all" >> "$HOME/.bashrc"
 # source bashrc
 echo "Sourcing bashrc..."
 source "$HOME/.bashrc"
+choice() {
+	echo "Would you like to put an executable command for xzpaque in /usr/bin or /usr/local/bin?
+	1. /usr/bin
+	2. /usr/local/bin
+	3. No(ne)./I will make an executable later."
+	read i_choice
+	case "$i_choice" in
+		1)
+			sudo cp $HOME/.paque/xzpaque/main.sh /usr/bin/xzpaque
+			sudo chmod +x /usr/bin/xzpaque
+		;;
+		2)
+			sudo cp $HOME/.paque/xzpaque/main.sh /usr/local/bin/xzpaque
+			sudo chmod +x /usr/local/bin/xzpaque
+		;;
+		3)
+			# do nothing
+		;;
+		*)
+			echo "please enter a valid answer. [1/2/3]"
+		;;
+	esac 
+}
 # done
 echo "Done!"
